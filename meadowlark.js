3927b2616ae2e4af04210fc752767c26e30d0503
Justin Tuttle <reddhouse@gmail.com>
2016-02-11 08:02:49 -0800

    Implement changes up through chapter 7

--- meadowlark.js
+++ meadowlark.js
@@ -1,29 +1,104 @@
 var express = require('express');
-
 var app = express();
 
 var fortune = require('./lib/fortune.js');
 
 //set up handlebars view engine
-var handlebars = require('express-handlebars')
-	.create({ defaultLayout:'main' });
+var handlebars = require('express-handlebars').create({
+	defaultLayout:'main',
+	helpers: {
+		section: function(name, options){
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+ 			return null;
+		}
+	}
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-
 // add static middleware
 app.use(express.static(__dirname + '/public'));
+
+// add middleware to detect test=1 in the query string
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+	next();
+});
+
+// used in example for partial views
+function getWeatherData(){
+    return {
+        locations: [
+            {
+                name: 'Portland',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            },
+            {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            },
+            {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            },
+        ],
+    };
+}
+
+// middleware to add weather data to context (res.locals.partials object)
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+ 	res.locals.partials.weatherContext = getWeatherData();
+ 	next();
+});
 
 //establish the routes
 app.get('/', function(req, res) {
 	res.render('home');
 });
-
 app.get('/about', function(req, res) {
-	res.render('about', { fortune: fortune.getFortune() } );
+	res.render('about', { 
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js' 
+	} );
 });
+app.get('/tours/hood-river', function(req, res) {
+	res.render('tours/hood-river');
+});
+app.get('/tours/oregon-coast', function(req, res){
+	res.render('tours/oregon-coast');
+});
+app.get('/tours/request-group-rate', function(req, res) {
+	res.render('tours/request-group-rate');
+});
+app.get('/jquery-test', function(req, res){
+	res.render('jquery-test');
+});
+app.get('/nursery-rhyme', function(req, res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+	res.json({
+		animal: 'squirrel',
+		bodyPart: 'tail',
+		adjective: 'bushy',
+		noun: 'heck',
+	});
+});
+
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next) {
@@ -42,3 +117,8 @@ app.listen(app.get('port'), function() {
 	console.log( 'Express started on http://localhost:' +
 		app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+
+
+
+
